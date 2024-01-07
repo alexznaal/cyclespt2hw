e1bbc03ac2ff76450a11acbb7ff17b954083fdc1
Alex <alxsandin@gmail.com>
2024-01-08 00:16:34 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -37,5 +37,51 @@ public class Main {
             countryPopulation = countryPopulation+addCount;
         }
         System.out.println("Год "+i+" численность населения составляет "+countryPopulation);
+
+        //task4,5
+        System.out.println("task4,5");
+
+        int vasiliMoney = 15000;
+        int vasiliMonth = 0;
+        while (vasiliMoney < 12000000) {
+            vasiliMonth = vasiliMonth+1;
+            vasiliMoney = vasiliMoney + vasiliMoney/100*7;
+            if ((vasiliMonth % 6) == 0) {
+                System.out.println(vasiliMonth+" месяц, сумма: "+vasiliMoney);
+            }
+        }
+
+        //task6
+        System.out.println("task6");
+
+        int vasiliMoney2 = 15000;
+        int vasiliMonth2 = 0;
+        while (vasiliMonth2 < 108) {
+            vasiliMonth2 = vasiliMonth2 + 1;
+            vasiliMoney2 = vasiliMoney2 + vasiliMoney2/100*7;
+            if ((vasiliMonth2 % 6) == 0) {
+                System.out.println(vasiliMonth2+" месяц, сумма накоплений: "+ vasiliMoney2);
+            }
+        }
+
+        //task7
+        System.out.println("task7");
+
+        int friday = 5;
+        while (friday <= 31) {
+            System.out.println("Сегодня пятница, "+ friday);
+            friday = friday + 7;
+        }
+
+        //task8
+        System.out.println("task8");
+
+        for (int j = 0; j <= 2124; j++) {
+            if ((j % 79) == 0) {
+                if (j > 1823 && j < 2124) {
+                    System.out.println(j);
+                }
+            }
+        }
     }
 }
